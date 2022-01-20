0bf6eb711739b80402206663c117e7c773fbeba8
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-01-20 16:13:20 +0530

    #include<stdio.h> int main() {     int n,r,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=i;j++)         {         printf("%d",j);         }         printf(" ");     } }

--- print the following pattern.c
+++ print the following pattern.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,arr[100];
+    int n,r,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
     {
-        scanf("%d",&n);
-    }
-    for(i=0;i<n;i++)
-    {
-        for(j=n;j>=1;j--)
+        for(j=1;j<=i;j++)
         {
-            printf("%d ",j);
+        printf("%d",j);
         }
         printf("
 ");
