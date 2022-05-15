b8284ed762d9b5809f45cc1f4cfa0bc118a02c5b
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-05-15 20:01:38 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,c,j;     float s=0,k=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);//1 2 3 4 5 6 2     }     for(i=0;i<n;i++)//i=1<7     {         c=0;//c=0         for(j=0;j<n;j++)//j=0<7         {             if(a[i]==a[j])//a[1]=a[0]             {                 c++;//c=1                 if(i!=j)                 {                 	a[j]=0; 				}             }         }         if(a[i]==c)//a[0]=1         {             s+=a[i];//s=1             k++;//k=1         }     }     if(k==0)     {         printf("-1");     }     else     {         printf("%.2f",s/k);     } }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -1,38 +1,40 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
-    float res=0,sum=0,count=0;
+    int n;
     scanf("%d",&n);
-    float arr[n];
+    int a[n],i,c,j;
+    float s=0,k=0;
     for(i=0;i<n;i++)
     {
-        scanf("%f",&arr[i]);//1 2 3 4 5 6 2
+        scanf("%d",&a[i]);//1 2 3 4 5 6 2
     }
-    for(i=0;i<n;i++)//i=0<7
+    for(i=0;i<n;i++)//i=1<7
     {
-        int t=0,x=arr[i];//x=1
+        c=0;//c=0
         for(j=0;j<n;j++)//j=0<7
         {
-            if(arr[i]==arr[j])//1==1
+            if(a[i]==a[j])//a[1]=a[0]
             {
-                t++;//t=1
+                c++;//c=1
+                if(i!=j)
+                {
+                	a[j]=0;
+				}
             }
         }
-        if(t==x)//1==1
+        if(a[i]==c)//a[0]=1
         {
-            sum+=arr[i];//sum=1
-            count++;//count=1
-            arr[i]=0;
+            s+=a[i];//s=1
+            k++;//k=1
         }
     }
-    if(count==0)
+    if(k==0)
     {
-        printf("%d",-1);
+        printf("-1");
     }
     else
     {
-        res=sum/count;
-        printf("%.2f",res);
+        printf("%.2f",s/k);
     }
 }
