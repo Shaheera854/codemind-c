5f9b8d9c761f0abd352a6184164004c1a6ee1d23
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 00:40:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n;j++)         {             if(j==i || j==1 || i==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,24 +1,22 @@
-#include <stdio.h>  
-  
-int main()  
-{  
-    int n;  
-    scanf("%d",&n);  
-    for(int i=n;i>=1;i--)  
-    {  
-        for(int j=1;j<=i-1;j++)  
-        {  
-            printf(" ");  
-        }  
-        for(int k=1;k<=n;k++)  
-        {  
-           if(i==1 || i==n || k==1 || k==n)  
-            printf("*");  
-            else  
-            printf(" ");   
-        }  
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j==i || j==1 || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
         printf("
-");  
-    }  
-    return 0;  
+");
+    }
 }
